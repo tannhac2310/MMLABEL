630612d45a7e221102e11431a7c640e3dc6ac8d3
gianglh <hoanggiangco94@gmail.com>
2023-11-05 22:54:48 +0700

    update

---
+++ internal/aurora/controller/customer.go
@@ -0,0 +1,187 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"mmlabel.gitlab.com/mm-printing-backend/internal/aurora/dto"
+	"mmlabel.gitlab.com/mm-printing-backend/internal/aurora/repository"
+	"mmlabel.gitlab.com/mm-printing-backend/internal/aurora/service/customer"
+	"mmlabel.gitlab.com/mm-printing-backend/pkg/apperror"
+	"mmlabel.gitlab.com/mm-printing-backend/pkg/interceptor"
+	"mmlabel.gitlab.com/mm-printing-backend/pkg/routeutil"
+	"mmlabel.gitlab.com/mm-printing-backend/pkg/transportutil"
+)
+
+type CustomerController interface {
+	CreateCustomer(c *gin.Context)
+	EditCustomer(c *gin.Context)
+	DeleteCustomer(c *gin.Context)
+	FindCustomers(c *gin.Context)
+}
+
+type customerController struct {
+	customerService customer.Service
+}
+
+func (s customerController) CreateCustomer(c *gin.Context) {
+	req := &dto.CreateCustomerRequest{}
+	err := c.ShouldBind(req)
+	if err != nil {
+		transportutil.Error(c, apperror.ErrInvalidArgument.WithDebugMessage(err.Error()))
+		return
+	}
+
+	userID := interceptor.UserIDFromCtx(c)
+
+	id, err := s.customerService.CreateCustomer(c, &customer.CreateCustomerOpts{
+		Name:        req.Name,
+		Avatar:      req.Avatar,
+		PhoneNumber: req.PhoneNumber,
+		Email:       req.Email,
+		Status:      req.Status,
+		Type:        req.Type,
+		Address:     req.Address,
+		CreatedBy:   userID,
+	})
+	if err != nil {
+		transportutil.Error(c, err)
+		return
+	}
+
+	transportutil.SendJSONResponse(c, &dto.CreateCustomerResponse{
+		ID: id,
+	})
+}
+
+func (s customerController) EditCustomer(c *gin.Context) {
+	req := &dto.EditCustomerRequest{}
+	err := c.ShouldBind(req)
+	if err != nil {
+		transportutil.Error(c, apperror.ErrInvalidArgument.WithDebugMessage(err.Error()))
+		return
+	}
+
+	err = s.customerService.EditCustomer(c, &customer.EditCustomerOpts{
+		ID:          req.ID,
+		Name:        req.Name,
+		Avatar:      req.Avatar,
+		PhoneNumber: req.PhoneNumber,
+		Email:       req.Email,
+		Status:      req.Status,
+		Type:        req.Type,
+		Address:     req.Address,
+	})
+	if err != nil {
+		transportutil.Error(c, err)
+		return
+	}
+
+	transportutil.SendJSONResponse(c, &dto.EditCustomerResponse{})
+}
+
+func (s customerController) DeleteCustomer(c *gin.Context) {
+	req := &dto.DeleteCustomerRequest{}
+	err := c.ShouldBind(req)
+	if err != nil {
+		transportutil.Error(c, apperror.ErrInvalidArgument.WithDebugMessage(err.Error()))
+		return
+	}
+
+	err = s.customerService.Delete(c, req.ID)
+	if err != nil {
+		transportutil.Error(c, err)
+		return
+	}
+
+	transportutil.SendJSONResponse(c, &dto.DeleteCustomerResponse{})
+}
+
+func (s customerController) FindCustomers(c *gin.Context) {
+	req := &dto.FindCustomersRequest{}
+	err := c.ShouldBind(req)
+	if err != nil {
+		transportutil.Error(c, apperror.ErrInvalidArgument.WithDebugMessage(err.Error()))
+		return
+	}
+
+	customers, cnt, err := s.customerService.FindCustomers(c, &customer.FindCustomersOpts{
+		Name: req.Filter.Name,
+	}, &repository.Sort{
+		Order: repository.SortOrderASC,
+		By:    "ID",
+	}, req.Paging.Limit, req.Paging.Offset)
+	if err != nil {
+		transportutil.Error(c, err)
+		return
+	}
+
+	customerResp := make([]*dto.Customer, 0, len(customers))
+	for _, f := range customers {
+		customerResp = append(customerResp, toCustomerResp(f))
+	}
+
+	transportutil.SendJSONResponse(c, &dto.FindCustomersResponse{
+		Customers: customerResp,
+		Total:     cnt.Count,
+	})
+}
+
+func toCustomerResp(f *customer.Data) *dto.Customer {
+	return &dto.Customer{
+		ID:          f.ID,
+		Name:        f.Name,
+		Avatar:      f.Avatar.String,
+		PhoneNumber: f.PhoneNumber.String,
+		Email:       f.Email.String,
+		Status:      f.Status,
+		Type:        f.Type,
+		Address:     f.Address.String,
+	}
+}
+
+func RegisterCustomerController(
+	r *gin.RouterGroup,
+	customerService customer.Service,
+) {
+	g := r.Group("customer")
+
+	var c CustomerController = &customerController{
+		customerService: customerService,
+	}
+
+	routeutil.AddEndpoint(
+		g,
+		"create",
+		c.CreateCustomer,
+		&dto.CreateCustomerRequest{},
+		&dto.CreateCustomerResponse{},
+		"Create customer",
+	)
+
+	routeutil.AddEndpoint(
+		g,
+		"edit",
+		c.EditCustomer,
+		&dto.EditCustomerRequest{},
+		&dto.EditCustomerResponse{},
+		"Edit customer",
+	)
+
+	routeutil.AddEndpoint(
+		g,
+		"delete",
+		c.DeleteCustomer,
+		&dto.DeleteCustomerRequest{},
+		&dto.DeleteCustomerResponse{},
+		"delete customer",
+	)
+
+	routeutil.AddEndpoint(
+		g,
+		"find",
+		c.FindCustomers,
+		&dto.FindCustomersRequest{},
+		&dto.FindCustomersResponse{},
+		"Find customers",
+	)
+}
--- internal/aurora/service/customer/customer.go
+++ internal/aurora/service/customer/customer.go
@@ -2,41 +2,38 @@ package customer
 
 import (
 	"context"
+
 	"github.com/go-redis/redis"
 
 	"mmlabel.gitlab.com/mm-printing-backend/internal/aurora/configs"
-	repository2 "mmlabel.gitlab.com/mm-printing-backend/internal/aurora/repository"
-	"mmlabel.gitlab.com/mm-printing-backend/pkg/repository"
+	"mmlabel.gitlab.com/mm-printing-backend/internal/aurora/repository"
 )
 
 type Service interface {
 	CreateCustomer(ctx context.Context, opt *CreateCustomerOpts) (string, error)
 	EditCustomer(ctx context.Context, opt *EditCustomerOpts) error
-	FindCustomers(ctx context.Context, opts *FindCustomersOpts, sort *repository2.Sort, limit, offset int64) ([]*Data, *repository2.CountResult, error)
-	Delete(ctx context.Context, id, userID string) error
+	FindCustomers(ctx context.Context, opts *FindCustomersOpts, sort *repository.Sort, limit, offset int64) ([]*Data, *repository.CountResult, error)
+	Delete(ctx context.Context, id string) error
 }
 
 type customerService struct {
-	customerRepo   repository2.CustomerRepo
-	permissionRepo repository.PermissionRepo
-	cfg            *configs.Config
-	redisDB        redis.Cmdable
+	customerRepo repository.CustomerRepo
+	cfg          *configs.Config
+	redisDB      redis.Cmdable
 }
 
 func NewService(
-	customerRepo repository2.CustomerRepo,
+	customerRepo repository.CustomerRepo,
 	cfg *configs.Config,
-	permissionRepo repository.PermissionRepo,
 	redisDB redis.Cmdable,
 ) Service {
 	return &customerService{
-		customerRepo:   customerRepo,
-		permissionRepo: permissionRepo,
-		cfg:            cfg,
-		redisDB:        redisDB,
+		customerRepo: customerRepo,
+		cfg:          cfg,
+		redisDB:      redisDB,
 	}
 }
 
 type Data struct {
-	*repository2.CustomerData
+	*repository.CustomerData
 }
