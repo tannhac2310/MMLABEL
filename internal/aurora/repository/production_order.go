ef7b247f70f625966c958b8430be831c4a745e72
gianglh <hoanggiangco94@gmail.com>
2023-12-07 04:13:48 +0700

    filter-po-by-status

--- internal/aurora/repository/production_order.go
+++ internal/aurora/repository/production_order.go
@@ -113,6 +113,11 @@ func (s *SearchProductionOrdersOpts) buildQuery(isCount bool, isAnalysis bool) (
 		conds += fmt.Sprintf(" AND b.%s = $%d", model.ProductionOrderFieldCustomerID, len(args))
 	}
 
+	if s.Status > 0 {
+		args = append(args, s.Status)
+		conds += fmt.Sprintf(" AND b.%s = $%d", model.ProductionOrderFieldStatus, len(args))
+	}
+
 	if len(s.Statuses) > 0 {
 		args = append(args, s.Statuses)
 		conds += fmt.Sprintf(" AND b.%s = ANY($%d)", model.ProductionOrderFieldStatus, len(args))
