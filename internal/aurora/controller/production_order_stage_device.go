131aca7ff49d7cd4685aa22813c2ef394b57f46c
minhvn <minhvo.me@gmail.com>
2023-12-08 05:56:52 +0700

    update field StageId

--- internal/aurora/controller/production_order_stage_device.go
+++ internal/aurora/controller/production_order_stage_device.go
@@ -60,7 +60,7 @@ func (s productionOrderStageDeviceController) FindProcessDeviceHistory(c *gin.Co
 			ID:                           deviceProcessStatusHistory.ID,
 			ProductionOrderStageDeviceID: deviceProcessStatusHistory.ProductionOrderStageDeviceID,
 			DeviceID:                     deviceProcessStatusHistory.DeviceID,
-			StageID: 					  deviceProcessStatusHistory.ProductionOrderStageDeviceID,
+			StageID: 					  deviceProcessStatusHistory.StageID.String,
 			ProcessStatus:                deviceProcessStatusHistory.ProcessStatus,
 			IsResolved:                   deviceProcessStatusHistory.IsResolved,
 			UpdatedAt:                    deviceProcessStatusHistory.UpdatedAt.Time,
