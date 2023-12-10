55d9e25ab0cef3258b3ea2843ed958c9bc97b772
gianglh <hoanggiangco94@gmail.com>
2023-12-10 10:21:39 +0700

    update

--- deployments/scripts/init_s3_bucket.sh
+++ deployments/scripts/init_s3_bucket.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 mc config host add minio http://s3_storage:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
 mc mb minio/${INIT_BUCKET_NAME}
+mc anonymous download minio/mm-printing-images
+mc anonymous set download mm-printing-images
+mc anonymous set public mm-printing-images
+mc anonymous list mm-printing-images
+
 mc policy download minio/${INIT_BUCKET_NAME}
+  mc anonymous list mm-printing-images
--- deployments/scripts/setup_s3.bash
+++ deployments/scripts/setup_s3.bash
@@ -8,7 +8,9 @@ if [[ "$(kubectl get namespace | grep -c $NAMESPACE)" != "1" ]]; then
 fi
 
 if [[ "$(helm repo ls | grep -c minio)" != "1" ]]; then
-		helm repo add minio https://helm.min.io/
+#		helm repo add minio https://helm.min.io/
+#		helm repo add minio2 https://charts.min.io/
+#    helm repo add minio https://helm.min.io/
 		helm repo update
 fi
 
@@ -16,8 +18,8 @@ case $ENV in
 
   "dev")
 		helm upgrade --install s3-storage -n $NAMESPACE \
-			minio/minio \
-			-f ./deployments/helm/s3/values.yaml
+			oci://registry-1.docker.io/bitnamicharts/minio \
+			-f ./deployments/helm/s3/values2.yaml
     ;;
 
   "staging")
