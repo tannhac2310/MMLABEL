44ce57d42c276b9b3ec3128848a62a509bfda27b
gianglh <hoanggiangco94@gmail.com>
2023-12-17 07:58:10 +0700

    localdeploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker login -u hoanggiangco94 -p "glpat-y5xdxy7rNb5uCsEin_Af" "registry.gitlab.com/mmlabel/mm-printing-web"
-docker build -t registry.gitlab.com/mmlabel/mm-printing-backend:${TAG} .
+docker build -t registry.gitlab.com/mmlabel/mm-printing-backend:${TAG} --build-arg VERSION=${TAG} .
 docker push registry.gitlab.com/mmlabel/mm-printing-backend:${TAG}
 
 git tag ${TAG}
