53833c8bd8687299cb77537066b1f6c2d50bb5ed
gianglh <hoanggiangco94@gmail.com>
2023-12-25 13:26:17 +0700

    update

--- internal/aurora/model/event_log.go
+++ internal/aurora/model/event_log.go
@@ -18,14 +18,14 @@ const (
 )
 
 type EventLog struct {
-	ID          int64                           `db:"id"`
-	DeviceID    string                          `db:"device_id"`
-	StageID     sql.NullString                  `db:"stage_id"`
-	StageStatus enum.ProductionOrderStageStatus `db:"stage_status"`
-	Quantity    float64                         `db:"quantity"`
-	Msg         sql.NullString                  `db:"msg"`
-	Date        sql.NullString                  `db:"date"`
-	CreatedAt   time.Time                       `db:"created_at"`
+	ID          int64                            `db:"id"`
+	DeviceID    string                           `db:"device_id"`
+	StageID     sql.NullString                   `db:"stage_id"`
+	StageStatus *enum.ProductionOrderStageStatus `db:"stage_status"`
+	Quantity    float64                          `db:"quantity"`
+	Msg         sql.NullString                   `db:"msg"`
+	Date        sql.NullString                   `db:"date"`
+	CreatedAt   time.Time                        `db:"created_at"`
 }
 
 func (rcv *EventLog) FieldMap() (fields []string, values []interface{}) {
