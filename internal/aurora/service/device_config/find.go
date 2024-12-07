6ee59efbf191462c070511d85d264b4cd6429d7f
gianglh <hoanggiangco94@gmail.com>
2024-12-07 15:42:37 +0700

    fix device ink

--- internal/aurora/service/device_config/find.go
+++ internal/aurora/service/device_config/find.go
@@ -2,8 +2,10 @@ package device_config
 
 import (
 	"context"
+	"fmt"
 
 	"mmlabel.gitlab.com/mm-printing-backend/internal/aurora/repository"
+	"mmlabel.gitlab.com/mm-printing-backend/pkg/database/cockroach"
 )
 
 func (c *deviceConfigService) FindDeviceConfigs(ctx context.Context, opts *FindDeviceConfigsOpts, sort *repository.Sort, limit, offset int64) ([]*Data, *repository.CountResult, error) {
@@ -26,9 +28,38 @@ func (c *deviceConfigService) FindDeviceConfigs(ctx context.Context, opts *FindD
 	if err != nil {
 		return nil, nil, err
 	}
+	inkIds := make([]string, 0, len(deviceConfigs))
+	for _, deviceConfig := range deviceConfigs {
+		if deviceConfig.InkID.String != "" {
+			inkIds = append(inkIds, deviceConfig.InkID.String)
+		}
+	}
+
+	inkData, err := c.inkRepo.Search(ctx, &repository.SearchInkOpts{
+		IDs:    inkIds,
+		Limit:  1000,
+		Offset: 0,
+	})
+	if err != nil {
+		return nil, nil, fmt.Errorf("lấy danh sách mực thất bại: %w", err)
+	}
+
+	inkMap := make(map[string]*repository.InkData, len(inkData))
+	for _, ink := range inkData {
+		inkMap[ink.ID] = ink
+	}
 
 	results := make([]*Data, 0, len(deviceConfigs))
 	for _, deviceConfig := range deviceConfigs {
+		tenLoaiMuc := ""
+		if deviceConfig.InkID.String != "" {
+			ink, ok := inkMap[deviceConfig.InkID.String]
+			if ok {
+				tenLoaiMuc = ink.Name
+			}
+		}
+		deviceConfig.TenLoaiMuc = cockroach.String(tenLoaiMuc)
+
 		results = append(results, &Data{
 			ProductionOrderDeviceConfigData: deviceConfig,
 		})
