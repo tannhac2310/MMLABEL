f1655a3848c3f21aa0abceb43d3dedd1b40559b0
21520372 <21520372@gm.uit.edu.vn>
2025-03-19 14:57:09 +0700

    Update OEE Add By Device and By Assigned Working

--- internal/aurora/dto/oee.go
+++ internal/aurora/dto/oee.go
@@ -1,18 +1,25 @@
 package dto
 
-import "time"
+import (
+	"time"
+)
 
 type FindOEERequest struct {
 	DateFrom string `json:"dateFrom"`
 	DateTo   string `json:"dateTo"`
 }
 
-type FindOEEResponse struct {
-	OEEList []OEE `json:"oeeList"`
-	Total   int64 `json:"total"`
+type FindOEEByDeviceResponse struct {
+	OEEList []OEEByDevice `json:"oeeList"`
+	Total   int64         `json:"total"`
 }
 
-type OEE struct {
+type FindOEEByAssignedWorkResponse struct {
+	OEEList []OEEByAssignedWork `json:"oeeList"`
+	Total   int64               `json:"total"`
+}
+
+type OEEByDevice struct {
 	DeviceID           string            `json:"deviceID"`
 	ActualWorkingTime  int64             `json:"actualWorkingTime"`
 	JobRunningTime     int64             `json:"jobRunningTime"`
@@ -35,4 +42,19 @@ type AssignedWork struct {
 	EstimatedCompleteAt    time.Time `json:"estimatedCompleteAt"`
 	Quantity               int64     `json:"quantity"`
 	Defective              int64     `json:"defective"`
+}
+
+type OEEByAssignedWork struct {
+	AssignedWorkID                string                `json:"assignedWorkID"`
+	ActualWorkingTime             int64                 `json:"actualWorkingTime"`
+	JobRunningTime                int64                 `json:"jobRunningTime"`
+	AssignedWorkTime              int64                 `json:"assignedWorkTime"`
+	DowntimeStatistics            map[string]string     `json:"downtimeStatistics"`
+	Availability                  float64               `json:"availability"`
+	Performance                   float64               `json:"performance"`
+	Quality                       float64               `json:"quality"`
+	TotalQuantity                 int64                 `json:"totalQuantity"`
+	TotalDefective                int64                 `json:"totalDefective"`
+	OEE                           float64               `json:"oee"`
+	DeviceProgressStatusHistories []DeviceStatusHistory `json:"deviceStatusHistory"`
 }
